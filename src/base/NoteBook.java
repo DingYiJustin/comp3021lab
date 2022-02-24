3c1048a4eccb56a7a0740d0fa5f49b0deb1222cc
DingYiJustin <yiyiding2002@126.com>
2022-02-24 10:45:49 +0800

    lab2 version1.0

---
+++ src/base/NoteBook.java
@@ -0,0 +1,47 @@
+package base;
+
+import java.util.ArrayList;
+
+public class NoteBook {
+	private ArrayList<Folder> folders;
+	
+	public NoteBook(){
+		folders = new ArrayList<Folder>(0);
+	}
+	
+	public boolean createTextNote(String folderName, String title) {
+		TextNote note = new TextNote(title);
+		return insertNote(folderName, note);
+	}
+	
+	public boolean createImageNote(String folderName, String title) {
+		ImageNote note = new ImageNote(title);
+		return insertNote(folderName, note);
+	}
+	
+	public ArrayList<Folder> getFolders(){
+		return folders;
+	}
+	
+	public boolean insertNote(String folderName, Note note) {
+		boolean checkIfExist = false;
+		for(Folder folder : folders) {
+			if(folder.equals(new Folder(folderName))) {
+				for(Note transNote : folder.getNotes()) {
+					if(note.equals(transNote)) {
+						System.out.println("Creating note "+ note.getTitle()+" under folder"+folderName+" failed");
+						return false;
+					}
+				}
+				folder.addNote(note);
+				return true;
+			}
+			
+		}
+		folders.add(new Folder(folderName));
+		folders.get(folders.size()-1).addNote(note);
+		return true;
+		
+	}
+	
+}
